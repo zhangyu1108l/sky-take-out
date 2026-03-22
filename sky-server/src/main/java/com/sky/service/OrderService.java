f13f131d27a114bd1c099d9d7436948330949b88
Liuye1216 <yeeee234@qq.com>
2026-03-22 19:37:05 +0800

    订单管理-订单搜索业务代码开发

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderSubmitVO;
@@ -32,4 +33,10 @@ public interface OrderService {
      * @param id
      */
     void userCancelById(Long id) throws Exception;
+    /**
+     * 条件搜索订单
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
 }
